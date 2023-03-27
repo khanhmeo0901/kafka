215ad93ae5659f66ec4981fc95d6983ce10f4e64
khanhmeo0901 <khanhvn09012000@gmail.com>
2023-03-27 11:00:55 +0700

    testGit

--- kafka03_23_2023/kafka/kafka_update/kafka-producer-consumer/src/main/java/com/ducnguyen/sbkafka/constant/ApplicationConstant.java
+++ kafka03_23_2023/kafka/kafka_update/kafka-producer-consumer/src/main/java/com/ducnguyen/sbkafka/constant/ApplicationConstant.java
@@ -5,4 +5,6 @@ public class ApplicationConstant {
     public static final String GROUP_ID = "group-id";
     public static final String TOPIC_EMPLOYEE = "topic_employee";
 
+    public static final String TOPIC_2703="topic_2703";
+
 }
